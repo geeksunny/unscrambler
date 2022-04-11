f9b16cc9be3f2d28416551638b802180fb27e07b
Justin Swanson <geeksunny@gmail.com>
2022-04-11 13:09:06 -0500

    Initial commit
    Base project started; Blank application with bottom navigation, initial fragments created.

---
+++ app/src/main/java/com/radicalninja/unscrambler/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.radicalninja.unscrambler
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.radicalninja.unscrambler.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_history, R.id.navigation_settings))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+}
---
+++ app/src/main/java/com/radicalninja/unscrambler/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.radicalninja.unscrambler.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+}
